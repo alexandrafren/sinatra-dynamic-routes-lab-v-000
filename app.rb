c2e5205fc93fb197e28e60c2363a4e17f06aa9b3
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-17 15:29:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,9 +23,9 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    @number1 = params[:number1]
+    @number1 = params[:number1].to_i
     @operation = params[:operation]
-    @number2 = params[:number2]
+    @number2 = params[:number2].to_i
     (@number1(@operation)@number2)
   end
 
