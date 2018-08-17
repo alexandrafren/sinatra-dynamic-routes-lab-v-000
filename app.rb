1b474f618bbaa13ab3d0d8453737e241c620d37f
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-17 15:18:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get "/reversename/:name"
+  get "/reversename/:name" do 
     @name = params[:name]
     @name.split("").reverse.join("")
   end
-
-end
 
 end
