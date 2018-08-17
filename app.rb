a33774736dc4b7465d5dd7c1a3cedf400aa8c97c
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-17 15:33:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
     @number1 = params[:number1].to_i
-    @operation = params[:operation],downcase
+    @operation = params[:operation].downcase
     @number2 = params[:number2].to_i
     case @operation
     when "add"
